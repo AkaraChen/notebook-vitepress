15877c6cbfb4a80db3a533b90ba9fd560a13aea8
AkaraChen <akarachen@outlook.com>
2022-06-13 15:31:04 +0800

    更新

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,20 +1,55 @@
 const defaultSidebar = [
     {
-        text: '编程语言',
+        text: 'JavaScript',
         collapsible: true,
         collapsed: true,
         items: [
-            { text: '类型系统', link: 'lang/type-system', },
-            { text: "基本类型", link: 'lang/types' },
+            { text: '技巧', link: 'javascript/skill/closure' },
+            { text: 'API', link: 'javascript/api/fetch' },
+            { text: '异步', link: 'javascript/async' },
+            { text: '后端', link: 'javascript/backend/express' },
+            { text: 'React', link: 'javascript/react/basic' },
+            { text: '面向对象', link: 'javascript/oop' },
+            { text: '函数式', link: 'javascript/fp' },
+            { text: 'TypeScript', link: 'javascript/ts' },
         ]
-    },
-    {
+    }, {
+        text: 'CSS',
+        collapsible: true,
+        collapsed: true,
+        items: [
+            { text: '杂项', link: 'css/miscellaneous' },
+            { text: '选择器', link: 'css/selector' },
+            { text: '相对单位', link: 'css/relative-unit' },
+            { text: '布局', link: 'css/layout' },
+            { text: '变换与动画', link: 'css/transform-animation' },
+            { text: '实例', link: 'css/snippets' },
+            { text: '设计', link: 'css/design' }
+        ]
+    }, {
+        text: 'Rust',
+        collapsible: true,
+        collapsed: true,
+        items: [
+            { text: '第一行代码', link: 'rust/headfirst' },
+            { text: '基础', link: 'rust/basic' },
+            { text: '所有权', link: 'rust/ownership' }
+        ]
+    }, {
         text: 'MySQL',
         collapsible: true,
         collapsed: true,
         items: [
             { text: '基础', link: 'mysql/basic' },
             { text: 'CRUD', link: 'mysql/crud' }
+        ]
+    }, {
+        text: '编程语言',
+        collapsible: true,
+        collapsed: true,
+        items: [
+            { text: '类型系统', link: 'lang/type-system', },
+            { text: "基本类型", link: 'lang/types' },
         ]
     }, {
         text: '操作系统',
@@ -29,12 +64,14 @@ const defaultSidebar = [
             { text: '内存虚拟化', link: 'system/vitual-memory' }
         ]
     }, {
-        text: 'Rust',
+        text: '机器学习',
         collapsible: true,
         collapsed: true,
         items: [
-            { text: '第一行代码', link: 'rust/headfirst' },
-            { text: '基础', link: 'rust/basic' }
+            { text: '第一行代码', link: 'ml/headfirst-in-js' },
+            { text: '历史', link: 'ml/history' },
+            { text: 'MNIST', link: 'ml/mnist' },
+            { text: 'GPU 加速', link: 'ml/gpu-support' }
         ]
     }, {
         text: '其他',
@@ -48,43 +85,7 @@ const defaultSidebar = [
             { text: 'Java', link: 'other/java/snippets' },
             { text: 'Golang', link: 'other/golang/basic' }
         ]
-    }, {
-        text: '机器学习',
-        collapsible: true,
-        collapsed: true,
-        items: [
-            { text: '第一行代码', link: 'ml/headfirst-in-js' },
-            { text: '历史', link: 'ml/history' },
-            { text: 'MNIST', link: 'ml/mnist' },
-            { text: 'GPU 加速', link: 'ml/gpu-support' }
-        ]
-    }, {
-        text: 'CSS',
-        collapsible: true,
-        collapsed: true,
-        items: [
-            { text: '杂项', link: 'css/miscellaneous' },
-            { text: '选择器', link: 'css/selector' },
-            { text: '相对单位', link: 'css/relative-unit' },
-            { text: '布局', link: 'css/layout' },
-            { text: '变换与动画', link: 'css/transform-animation' },
-            { text: '实例', link: 'css/snippets' },
-        ]
-    }, {
-        text: 'JavaScript',
-        collapsible: true,
-        collapsed: true,
-        items: [
-            { text: '技巧', link: 'javascript/skill/closure' },
-            { text: 'API', link: 'javascript/api/fetch' },
-            { text: '异步', link: 'javascript/async' },
-            { text: '后端', link: 'javascript/backend/express' },
-            { text: 'React', link: 'javascript/react/basic' },
-            { text: '面向对象', link: 'javascript/oop' },
-            { text: '函数式', link: 'javascript/fp' },
-            { text: 'TypeScript', link: 'javascript/ts' },
-        ]
-    }
+    },
 ]
 
 const JavaScriptSidebar = [
@@ -127,7 +128,7 @@ const JavaScriptSidebar = [
         collapsed: true,
         items: [
             { text: '基础', link: 'javascript/react/basic' },
-            { text: 'Hooks', link: 'javascript/react/hooks'}
+            { text: 'Hooks', link: 'javascript/react/hooks' }
         ]
     },
     {
